ffd815078dcc1e1a08af82af5a7dedd294c80b8f
Juliana R. Mancera <133284711+JulianaMancera@users.noreply.github.com>
2025-02-24 02:27:28 +0000

    challenge 4

--- challenge_lab4.php
+++ challenge_lab4.php
@@ -2,17 +2,12 @@
 # Lab Challenge #4
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 == 0 && $i % 5 == 0) {
-        echo "$i is divisible to 3 and 5";
-        echo '<br>';
+        echo $i . ' is divisible to 3 and 5 <br>';
     } elseif ($i % 3 == 0) {
-        echo "$i is divisible to 3";
-        echo '<br>';
+        echo $i . ' is divisible to 3 <br>';
     } elseif ($i % 5 == 0) {
-        echo "$i is divisible to 5";
-        echo '<br>';
+        echo $i .' is divisible to 5 <br>';
     } else {
-        echo "$i";
-        echo '<br>';
+        echo $i . '<br>';
     }
 }
-
